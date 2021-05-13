543a8e699920e3d3f26b02eb220bc294447101b9
Patel27-S <smit4632@gmail.com>
2021-05-13 12:09:14 -0400

    The first file is added to the repository.

---
+++ num_unique.py
@@ -0,0 +1,36 @@
+'''The program will determine whether an input of sequence 
+    of numbers are all unique or not.
+    '''
+
+def find_uniqueness(List):
+    '''This function finds if the elements
+        are unique in the List.
+        '''
+    if len(List) == len(set(List)):  #  Set doesn't have duplicates.
+        return True
+    else:
+        return False
+
+
+# The Time Complexity of the below function is O(n^2).
+
+def find_uniqueness2(List):
+    '''This function determines the uniqueness
+        of elements in the List.
+        '''
+    for i in range(0, len(List)-1):  
+        for j in range(i+1, len(List)):
+            if List[i] == List[j]:  # Equal value condition while comparing
+                return False
+    return True
+
+
+
+            
+
+
+
+
+
+
+
