50122ffefea9ba81e41fea7423e2dd5eb4f102f7
Smit Patel <81384943+Patel27-S@users.noreply.github.com>
2021-06-28 17:40:23 -0400

    In this version of commit some practiced tuple and dictionary codes have been put in.

---
+++ Dictionary/oldest_student.py
@@ -0,0 +1,18 @@
+'''
+This program has a function which returns the oldest student of the given students'
+name and age pairs in the form of dictionary.
+'''
+
+
+def oldest_stu(dictionary):
+    '''
+    Returns the oldest student when a dictionary of
+    students and their age value pairs are input.
+    '''
+    largest = -1
+    oldest_std = None
+    for student,age in dictionary.items():  # This will iterate through all the student,age pairs in the dictionary.
+        if age > largest:
+            largest = age
+            oldest_std = student
+    return (oldest_std, largest)
